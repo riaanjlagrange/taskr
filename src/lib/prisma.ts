0cc17eb427266ce0a76917ff2b12c2f7ab5939f7
riaanjlagrange <riaanjlagrange@gmail.com>
2025-11-23 14:23:37 +0200

    Set up SQLite, Prisma & define Prisma schema

---
+++ src/lib/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaClient } from '../generated/prisma/client';
+
+const adapter = new PrismaBetterSqlite3({
+  url: "file:./prisma/app.db"
+})
+const prisma = new PrismaClient({ adapter })
+
+export default prisma;
