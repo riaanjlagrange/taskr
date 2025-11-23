67ed2b3cee00b2d72d8057822e2b86718ceba648
riaanjlagrange <riaanjlagrange@gmail.com>
2025-11-23 16:45:18 +0200

    Set up UI layout & install shadcn

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,7 @@
-const Footer = () => {
+export default function Footer() {
   return (
-    <footer className="flex items-center justify-center p-4">
+    <footer className="flex items-center justify-center p-4 w-full fixed bottom-0 bg-zinc-900">
       Footer
     </footer>
   )
 }
-
-export default Footer
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,14 @@
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
 
-const Header = () => {
+export default function Header() {
   return (
-    <header className="flex justify-end items-center p-4 gap-4 h-16">
+    <header className="flex justify-end items-center p-4 gap-4 h-16 w-full fixed top-0 bg-zinc-900">
       <SignedOut>
-	<SignInButton />
+	<SignInButton>
+	  <button className="text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+	    Sign In
+	  </button>
+	</SignInButton>
 	<SignUpButton>
 	  <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
 	    Sign Up
@@ -17,5 +21,3 @@ const Header = () => {
     </header>
   )
 }
-
-export default Header
