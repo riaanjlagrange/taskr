e6a0b56c6bfd27b2294ddbf7b799800076517615
riaanjlagrange <riaanjlagrange@gmail.com>
2025-11-24 18:01:52 +0200

    Update home page hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { currentUser } from "@clerk/nextjs/server";
 import Link from "next/link";
+import Image from "next/image";
+import { SignIn, SignInButton, SignUpButton } from "@clerk/nextjs";
 
 export default async function Home() {
   // get current user from clerk
@@ -18,13 +20,34 @@ export default async function Home() {
 	</Link>
       </div>
     )
-    return <p>Please sign in to start</p>
+    return (
+      <div className="flex gap-2">
+	<SignInButton>
+	  <Button variant="secondary">
+	    Sign In
+	  </Button>
+	</SignInButton>
+	<SignUpButton>
+	  <Button>
+	    Sign Up
+	  </Button>
+	</SignUpButton>
+      </div>
+    );
   }
 
   // else if user is signed in, show this
   return (
     <main className="lg:max-w-[50vw] md:max-w-[65vw] max-w-[80vw] mx-auto pt-8">
       <section className="w-full flex flex-col items-center justify-center">
+	{/* hero image */}
+	<Image
+	  src="/hero.svg"
+	  alt="Hero Image"
+	  width={300}
+	  height={300}
+	  className="mb-12"
+	/>
 	<h1 className="text-2xl">
 	  Welcome to <span className="text-indigo-300 font-bold">Taskr</span>
 	</h1>
