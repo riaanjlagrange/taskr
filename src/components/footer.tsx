0d944fb69b96b207a5cdd96738035e7e5cf52fef
riaanjlagrange <riaanjlagrange@gmail.com>
2025-11-24 13:01:54 +0200

    Update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,13 @@
+import { FaGithub } from "react-icons/fa6";
+
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center p-4 w-full fixed bottom-0 bg-zinc-900">
-      Footer
+    <footer className="flex items-center gap-2 justify-center p-4 w-full fixed bottom-0 bg-zinc-900">
+      <a href="https://github.com/riaanjlagrange" target="_blank" className="text-indigo-300 flex items-center gap-2 cursor-pointer">
+	<FaGithub fill="white" />
+	riaanjlagrange
+      </a>
+      <span className="talic text-zinc-200 text-sm">&copy; {new Date().getFullYear()}</span>
     </footer>
   )
 }
