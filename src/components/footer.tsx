1ae5139d8af1d3135704e761066b6af363b9d151
riaanjlagrange <riaanjlagrange@gmail.com>
2025-11-27 16:46:10 +0200

    Update demo gif & change footer url to the github repo for this project

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { FaGithub } from "react-icons/fa6";
 export default function Footer() {
   return (
     <footer className="flex items-center gap-2 justify-center p-4 w-full fixed bottom-0 bg-zinc-900">
-      <a href="https://github.com/riaanjlagrange" target="_blank" className="text-indigo-300 flex items-center gap-2 cursor-pointer">
+      <a href="https://github.com/riaanjlagrange/taskr" target="_blank" className="text-indigo-300 flex items-center gap-2 cursor-pointer">
 	<FaGithub fill="white" />
 	riaanjlagrange
       </a>
